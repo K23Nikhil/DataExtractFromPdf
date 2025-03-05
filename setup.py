53bb8e9cbe66e81cc845fe8e9836399390451aa9
Nikhil Kulshrestha <er.nikhil1992@gmail.com>
2025-03-05 15:15:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         # List your project's dependencies here.
-        'requests','json', 'panda'
+        're','json', 'pandas', 'PyPDF2'
     ],
 )
